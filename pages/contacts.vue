6255ca7c82d01dced27778b77798e2a203fbaa11
khaalidm <khaalidm.m@gmail.com>
2021-02-13 19:48:30 +0200

    initial commit

---
+++ pages/contacts.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Contacts</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'View Contacts',
+    }
+  },
+}
+</script>
+
+<style></style>
